6b89c98f3b68df81d3d0ee2221d0cd73b6c48231
John Flanigan <johnrflanigan@gmail.com>
2020-12-01 12:46:30 -0500

    Completed day 1

---
+++ src/day1.rs
@@ -0,0 +1,48 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs;
+
+pub fn run() {
+  let mut path = env::current_dir().expect("Error getting current directory");
+  path.push("src");
+  path.push("input");
+  path.push("day1_input.txt");
+  let filename = path.to_str().expect("Error getting path as string");
+  let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+  let nums: HashSet<i64> = contents
+    .lines()
+    .map(|line| line.parse::<i64>().unwrap())
+    .collect();
+
+  println!("Part one solution: {}", two_sum(nums.clone(), 2020));
+  println!("Part two solution: {}", three_sum(nums, 2020));
+}
+
+fn two_sum(nums: HashSet<i64>, val: i64) -> i64 {
+  let mut set: HashSet<i64> = HashSet::new();
+
+  for num in nums {
+    if set.contains(&(val - num)) {
+      return (val - num) * num;
+    } else {
+      set.insert(num);
+    }
+  }
+
+  return -1;
+}
+
+fn three_sum(nums: HashSet<i64>, val: i64) -> i64 {
+  for num in nums.clone() {
+    let mut temp = nums.clone();
+    temp.remove(&num);
+
+    let solution = two_sum(temp, val - num);
+    if solution != -1 {
+      return num * solution;
+    }
+  }
+
+  return -1;
+}
